d763bbec5291947ed8fba3d15a47dfb33358beae
Gstavo132 <gustavopedroso360@gmail.com>
2024-12-05 14:16:25 -0300

    Add files via upload

---
+++ calculadora/#Requisitos Funcionais.py
@@ -0,0 +1,11 @@
+#Requisitos Funcionais
+
+#A calculadora deve permitir ao usuário:
+
+#Realizar soma de dois números.
+
+#Realizar multiplicação de dois números.
+
+#Realizar divisão de dois números (com verificação de divisão por zero).
+
+#Deve exibir um menu interativo para o usuário escolher as operações.
---
+++ calculadora/README.md
@@ -0,0 +1,8 @@
+# Calculadora em Python
+Este é um projeto de uma calculadora simples feita em Python, que suporta operações básicas e pode ser expandida para operações avançadas.
+
+## Funcionalidades
+- Adição
+- Subtração
+- Multiplicação
+- Divisão
